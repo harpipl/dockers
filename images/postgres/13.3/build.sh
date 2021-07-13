3119dd6faec2b3333383fb8cd48ca1de06600fc2
harpi78 <arkadiusz.adamczak@gmail.com>
2021-07-13 18:59:13 +0200

    Change adarsoft to harpipl

--- images/postgres/13.3/build.sh
+++ images/postgres/13.3/build.sh
@@ -1,2 +1,2 @@
-docker build -t adarsoft.jfrog.io/common-docker/postgres:13.3.1 .
-docker push adarsoft.jfrog.io/common-docker/postgres:13.3.1
+docker build -t harpipl.jfrog.io/common-docker/postgres:13.3.1 .
+docker push harpipl.jfrog.io/common-docker/postgres:13.3.1
